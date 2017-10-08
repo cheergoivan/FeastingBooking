ff05db929c31b49a9c08b1d23e841da2e9919fce
cheergoivan <cheergoivan@IvansLaptop>
2017-10-08 22:17:45 +0800

    done

--- src/main/java/com/iplay/dto/hotel/SimplifiedHotelDTO.java
+++ src/main/java/com/iplay/dto/hotel/SimplifiedHotelDTO.java
@@ -5,16 +5,19 @@ public class SimplifiedHotelDTO {
 	private String name;
 	private double[] priceRange;
 	private int[] tableRange;
+	private String districtOfAddress;
 	private int numOfComment;
 	private String pictureUrl;
+	
 
-	public SimplifiedHotelDTO(int id, String name, double[] priceRange, int[] tableRange, int numOfComment,
+	public SimplifiedHotelDTO(int id, String name, double[] priceRange, int[] tableRange, String districtOfAddress, int numOfComment,
 			String pictureUrl) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.priceRange = priceRange;
 		this.tableRange = tableRange;
+		this.districtOfAddress = districtOfAddress;
 		this.numOfComment = numOfComment;
 		this.pictureUrl = pictureUrl;
 	}
@@ -65,5 +68,13 @@ public class SimplifiedHotelDTO {
 
 	public void setPictureUrl(String pictureUrl) {
 		this.pictureUrl = pictureUrl;
+	}
+
+	public String getDistrictOfAddress() {
+		return districtOfAddress;
+	}
+
+	public void setDistrictOfAddress(String districtOfAddress) {
+		this.districtOfAddress = districtOfAddress;
 	}
 }
